12edd9a8a9a55026065cae7e76aa8628a5da0414
krishnamaneni, chandrababu(ck9389) <ck9389@att.com>
2018-10-12 19:50:30 +0530

    Second time for loop adding committing.

--- Hello/src/com/kc/HelloTest.java
+++ Hello/src/com/kc/HelloTest.java
@@ -2,6 +2,9 @@ package com.kc;
 
 public class HelloTest {
 public static void main(String[] args) {
-	System.out.println("babu");
+	
+	for(int i=0;i<=10;i++) {
+	System.out.println("babu" +i);
+	}
 }
 }
